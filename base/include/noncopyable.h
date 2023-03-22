337c8e419b3ebd3f1885a3fee97a7657d57ac9bd
youarefree123 <1063961685@qq.com>
2023-03-22 15:36:39 +0800

    调通spdlog

--- base/include/noncopyable.h
+++ base/include/noncopyable.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// 继承自NonCopyable的类可以正常构造和析构，但是不能进程拷贝构造和拷贝赋值
 class NonCopyable {
 public:
   NonCopyable( const NonCopyable& ) = delete;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "buffer.h"
 #include "noncopyable.h"
-
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
 // #include <deque>
 // #include <iostream>
 using namespace std;
@@ -10,6 +11,13 @@ int main(int argc, char const *argv[])
   // deque<int> a{1};
   // cout<<a.front()<<endl;
   Buffer b{};
-  // b.show();  
+  // 开启并创建本地日志
+  auto  my_logger = spdlog::basic_logger_mt("file_logger", "../logs/basic-log");
+
+  // 设置该日志的显示级别
+  my_logger->set_level(spdlog::level::info);
+
+  // 向该日志中写入信息
+  my_logger->info("Hello, {}!", "World");
   return 0;
 }

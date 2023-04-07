bae2aa61adc06e2fd423644b24a824cb681a8c64
youarefree123 <1063961685@qq.com>
2023-04-07 00:34:32 -0700

    4.7 eventloop暂时修改

--- net/include/event_loop.h
+++ net/include/event_loop.h
@@ -1,17 +1,74 @@
 #pragma once 
 
+#include <vector>
+#include <memory>
+#include <mutex>
+#include <functional>
+
+#include "timestamp.h"
 #include "noncopyable.h"
+
+
+class Channel;
+class Poller;
 
 class EventLoop : noncopyable
 {
 
 public: 
-  
-    EventLoop(/* args */);
+    using std::function<void()> Functor;
+    EventLoop();
     ~EventLoop();
 
+    // 事件循环
+    void Loop(); 
+    // 关闭事件循环
+    void Quit();
+
+    // 轮询返回的时间，通常意味着有事件到达
+    Timestamp PollReturnTime const { 
+        return poll_return_time_;
+    }
+    // 立即执行一个回调
+    // 在其他线程中使用是安全的
+    void RunInLoop( Functor cb );
+
+    // 内部使用
+    void WakeUp();
+    void UpdateChannel( Channel* channel );
+    void RemoveChannel( Channel* channel );
+    bool HasChannel( Channel* channel );
+
+    bool IsInLoopThread() const {
+        return thread_id == CurrentThread::Tid();
+    }
+    // 断言，确认loop是否在对应线程里
+    void AssertInLoopThread() {
+        if( !IsIn )
+    }
 
 private:
+    using ChannelList = std::vector<Channel*>;
 
+    void AbortNotInLoopThread(); 
+    void HandleRead(); // waked up 
+    void DoPendingFunctors(); 
+
+    bool looping_; /* atomic , 是否在循环*/ 
+    std::atomic<bool> quit_;  /*是否已经终止循环*/
+    bool event_handling_; /* atomic, 是否有事件正在处理 */
+    bool calling_pending_functors_; /* atomic 是否正在调用回调？？ */
+    const pid_t thread_id; /* 当前loop的线程 */
+    Timestamp poll_return_time_; /* 记录本次poll的返回时间 */
+    std::unique_ptr<Poller> p_poller_; /*  */ 
+    int wakeup_fd_; /* 重要，唤醒subloop 的fd*/
+    std::unique_ptr<Channel> p_wakeup_channel_;  
+
+    // 暂存变量
+    ChannelList active_channels_; 
+    Channel* current_active_channel_;
+
+    std::mutex mtx_;
+    std::vector<Functor> pending_functors_; /* 待执行的回调 */
 };
 
--- net/include/inet_address.h
+++ net/include/inet_address.h
@@ -6,16 +6,16 @@ using std::string;
 
 class InetAddress {
 public:
-  explicit InetAddress( uint16_t port = 80, string ip = "127.0.0.1" );
-  explicit InetAddress( const InetAddress& addr ) 
-    : sockaddr_(addr.sockaddr()){} 
-  
-  string ToIp() const;
-  uint16_t ToPort() const;
-  string ToIpPort() const;
+    explicit InetAddress( uint16_t port = 80, string ip = "127.0.0.1" );
+    explicit InetAddress( const InetAddress& addr ) 
+        : sockaddr_(addr.sockaddr()){} 
+    
+    string ToIp() const;
+    uint16_t ToPort() const;
+    string ToIpPort() const;
 
-  const sockaddr_in& sockaddr() const { return sockaddr_; } 
+    const sockaddr_in& sockaddr() const { return sockaddr_; } 
 
 private:
-  sockaddr_in sockaddr_;
+    sockaddr_in sockaddr_;
 };

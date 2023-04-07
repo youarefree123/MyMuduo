78efa20bfce691f6d13d36303e1dfa687eb40613
youarefree123 <1063961685@qq.com>
2023-04-07 08:30:05 +0800

    4.7 本地提交，添加current_thread

--- main.cpp
+++ main.cpp
@@ -1,39 +1,50 @@
 #include "buffer.h"
 #include "noncopyable.h"
-// #include "poller.h"
-// #include "channel.h"
+#include "poller.h"
+#include "channel.h"
 #include "log.h"
-// #include <deque>
-// #include <iostream>
+#include "current_thread.h"
+
+
+
+
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
   // srcDir_ = getcwd(nullptr, 256);
   Buffer b{};
+    
+  ONLY_TO_CONSOLE; LOGINIT(); 
+  // ONLY_TO_FILE; LOGINIT(); 
+  LOG_LEVEL_INFO;
+
+  INFO("{}",CurrentThread::Tid());
   
 
-  ONLY_TO_FILE; LOGINIT(); LOG_LEVEL_INFO;
-  for( int i = 0; i < 10; i++ ) {
-    if( i < 5 ) {
-      LOG_LEVEL_INFO;
-      TRACE("trace{}",i);
-      DEBUG("debug{}",i);
-      INFO("info{}",i);
-      WARN("warn{}",i);
-      ERROR("error{}",i);
-      // CRITICAL( "crittcal{}",i );
-    }
-    else {
-      LOG_LEVEL_TRACE;
-      TRACE("trace{}",i);
-      DEBUG("debug{}",i);
-      INFO("info{}",i);
-      WARN("warn{}",i);
-      ERROR("error{}",i);
-      // CRITICAL( "crittcal{}",i );
-    }
-  }
+
+
+  // for( int i = 0; i < 10; i++ ) {
+  //   if( i < 5 ) {
+  //     LOG_LEVEL_INFO;
+  //     TRACE("trace{}",i);
+  //     DEBUG("debug{}",i);
+  //     INFO("info{}",i);
+  //     WARN("warn{}",i);
+  //     ERROR("error{}",i);
+  //     // CRITICAL( "crittcal{}",i );
+  //   }
+  //   else {
+  //     LOG_LEVEL_TRACE;
+  //     TRACE("trace{}",i);
+  //     DEBUG("debug{}",i);
+  //     INFO("info{}",i);
+  //     WARN("warn{}",i);
+  //     ERROR("error{}",i);
+  //     // CRITICAL( "crittcal{}",i );
+  //   }
+  // }
   
   return 0;
 }

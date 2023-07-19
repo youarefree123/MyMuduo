7f5ac6abbcc8ad4a02e2af3ee6259765a02342ae
youarefree123 <1063961685@qq.com>
2023-07-19 02:36:52 -0700

    编写googletest 测试样例，没有调通

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# WebServer
-WebServer
+# My Muduo
+
+## 克隆项目
+git clone --recursive https://github.com/youarefree123/MyMuduo.git
+
+## 编译
+
+```shell
+bash build.sh
+```
--- build.sh
+++ build.sh
@@ -2,16 +2,39 @@
 
 set -e
 
+# 清空
+# rm -rf `pwd`/build/*
+
+# 编译googletest
+cd `pwd`/3rdparty/googletest
+
+if [ ! -d `pwd`/build ]; then
+    mkdir `pwd`/build
+    cd build && 
+    cmake .. && 
+    make -j8 && 
+    sudo make install &&
+    sudo ldconfig
+    
+
+    # ubuntu 会额外创建一个/usr/local/lib/x86_64-linux-gnu文件夹
+    # 所以需要把库文件移出来
+    sudo cp -r /usr/local/lib/x86_64-linux-gnu/*.a /usr/local/lib/
+
+    # 回到主目录
+    cd ../../../
+fi
+
+
+
+
 # 如果没有build文件夹，就创建
 if [ ! -d `pwd`/build ]; then
     mkdir `pwd`/build
 fi
 
-# 清空
-# rm -rf `pwd`/build/*
-
 # 编译
 cd `pwd`/build &&
     cmake .. && 
     make -j8
-# cd ..
+cd ../
